33e251bf93fedcc54643b0033e853166325e706a
kibethh <kibethh@gmail.com>
2022-11-09 20:48:25 +0300

    22

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,4 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -28,9 +28,42 @@ impl TryFrom<&[u8]> for Request {
         // After implementing From for utf8 Error
         // ?-> Looks for From implementations
         let request = str::from_utf8(buf)?;
+        // match get_next_word(request) {
+        //     Some((method, request)) => {}
+        //     None => return Err(ParseError::InvalidRequest),
+        // }
+        // ok_or - converts an option into a result
+        // With value in Some(value) being wrapped by Ok(value)
+        // And error being wrapped by Err(Error)
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
         unimplemented!()
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    let mut iter = request.chars();
+    // loop {
+    //     let item = iter.next();
+    //     match item {
+    //         Some(c) => {}
+    //         None => break,
+    //     }
+    // }
+
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+    None
 }
 
 pub enum ParseError {
@@ -57,6 +90,11 @@ impl Display for ParseError {
 impl Debug for ParseError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         write!(f, "{}", self.message())
+    }
+}
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 impl From<Utf8Error> for ParseError {
